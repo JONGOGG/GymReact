0d7f49265c79e3cd9375b60911b1f497eccd8a25
Cristian Abraham De Luna Mercado <crisistian87@gmail.com>
2024-07-30 19:05:54 -0600

    diseno de la barra de navegacion

--- src/screens/ClientScreen.js
+++ src/screens/ClientScreen.js
@@ -4,12 +4,42 @@ import RutinasScreen from './RutinasScreen';
 import DietasScreen from './DietasScreen';
 import EjerciciosScreen from './EjerciciosScreen';
 import PerfilScreen from './PerfilScreen';
+import { Ionicons } from '@expo/vector-icons';
 
 const Tab = createBottomTabNavigator();
 
 const ClientScreen = () => {
   return (
-    <Tab.Navigator>
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ color, size }) => {
+          let iconName;
+
+          if (route.name === 'Rutinas') {
+            iconName = 'fitness-outline';
+          } else if (route.name === 'Dietas') {
+            iconName = 'nutrition-outline';
+          } else if (route.name === 'Ejercicios') {
+            iconName = 'barbell-outline';
+          } else if (route.name === 'Perfil') {
+            iconName = 'person-outline';
+          }
+
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
+        tabBarActiveTintColor: '#e91e63',
+        tabBarInactiveTintColor: 'gray',
+        tabBarStyle: {
+          backgroundColor: '#ffffff',
+          paddingBottom: 5,
+          height: 60,
+        },
+        tabBarLabelStyle: {
+          fontSize: 14,
+          fontWeight: 'bold',
+        },
+      })}
+    >
       <Tab.Screen name="Rutinas" component={RutinasScreen} />
       <Tab.Screen name="Dietas" component={DietasScreen} />
       <Tab.Screen name="Ejercicios" component={EjerciciosScreen} />
